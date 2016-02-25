a5255d7d048be06db8cab80a25e4a484192694a9
Matt Congel <congel@chapman.edu>
2016-02-25 13:43:47 -0800

    debug

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,16 +7,17 @@ set :use_sudo, false
 set :bundle_binstubs, nil
 
 set :port, 22
-set :user, 'congel'
+set :user, 'deployer'
 set :rbenv_ruby, '2.3.0'
 set :deploy_via, :remote_cache
 
 set :deploy_to, "/home/#{fetch(:user)}/#{fetch(:application)}"
+set :tmp_dir, "/home/deployer/tmp"
 
 set :ssh_options, {
   forward_agent: true,
   auth_methods: %w(publickey),
-  user: 'congel'
+  user: 'deployer'
 }
 
 set :rails_env, :production
@@ -27,24 +28,6 @@ server '45.55.29.180',
   port: fetch(:port),
   user: fetch(:user),
   primary: true
-
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
-
-# Default value for :scm is :git
-# set :scm, :git
-
-# Default value for :format is :pretty
-# set :format, :pretty
-
-# Default value for :log_level is :debug
-# set :log_level, :debug
-
-# Default value for :pty is false
-# set :pty, true
 
 # Default value for :linked_files is []
 set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
