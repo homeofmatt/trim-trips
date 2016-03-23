e00a188fa8f58bf8a5cbce30f775abea7e5fe895
Matt Congel <congel@chapman.edu>
2016-03-23 14:20:07 -0700

    debug

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -11,7 +11,7 @@ preload_app true
 timeout 30
 
 # Set up socket location
-listen "#{shared_dir}/tmp/sockets/unicorn.sock", :backlog => 64
+listen "/tmp/sockets/unicorn.sock", :backlog => 64
 
 # Logging
 stderr_path "#{shared_dir}/tmp/log/unicorn.stderr.log"
